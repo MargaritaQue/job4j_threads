8293ed2d1e03bcdc341ec380f31e8087d6aba6e6
margarita <margarita.que@mail.ru>
2026-02-23 00:16:43 +0400

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 101; i++) {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + i + "%");
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+        );
+        thread.start();
+    }
+}
